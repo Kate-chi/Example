d00c3f2c221a405078ebd692beb30a12d25472d7
Kate <c.chistova2014@yandex.ru>
2022-10-17 17:37:04 +0300

    add IF/ELSE

---
+++ Example5_IF/ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "катя")
+{
+    Console.WriteLine("Катя вернулась!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
