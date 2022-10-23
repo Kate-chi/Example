8eaa3440efa3575c24368dbbdd00e688545c0424
Kate <c.chistova2014@yandex.ru>
2022-10-23 15:23:29 +0300

    пример функций и массива

---
+++ Example8_method/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int a, int b, int c)
+{
+    int result = a;
+    if(b > result) result = b;
+    if(c > result) result = c;
+    return result;
+}
+
+int x = 4;
+int y = 86;
+int z = 43;
+
+int max = Max(x, y, z);
+
+Console.WriteLine(max);
