eac8144234daacce031decc81c1285bcd3581166
Kate <c.chistova2014@yandex.ru>
2022-10-23 15:46:02 +0300

    поиск индекса элемента по значению

---
+++ Example10_method+array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {2, 9, 56, 95, 95, 4, 8};
+
+int n = array.Length;
+int index = 0;
+
+int find = 95;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
--- Example9_Array/Program.cs
+++ Example9_Array/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int Max(int a, int b, int c)
+{
+    int result = a;
+    if(b > result) result = b;
+    if(c > result) result = c;
+    return result;
+}
+
+int[] arr = {5, 9, 76};
+
+int max = Max(arr[0], arr[1], arr[2]);
+
+Console.WriteLine(max);
