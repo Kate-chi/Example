bf2040558bed9879348be8f49d63610f907fea0e
Kate <c.chistova2014@yandex.ru>
2022-10-27 17:34:44 +0300

    сортировка от максимального

---
+++ Example15_SortingMax/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] arr = {3, 7, 8, 4, 6, 2, 5, 7, 4, 1};
+
+void PrintArr(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortArr(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        int maxPos = i;
+
+        for(int j = i + 1; j < arr.Length; j++)
+        {
+            if(arr[j] > arr[maxPos])
+            {
+                maxPos = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[maxPos];
+        arr[maxPos] = temp;
+    }
+}
+
+
+PrintArr(arr);
+SortArr(arr);
+PrintArr(arr);
