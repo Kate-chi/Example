aa1458619b013f5788f98f987192187a84273b71
Kate <c.chistova2014@yandex.ru>
2022-10-27 17:20:17 +0300

    сортировка от минимального

---
+++ Example14_sortingMin/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] arr = {7, 5, 8, 3, 8, 4, 4, 5, 1, 2};
+
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SortArr(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        int minPos = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPos])
+            {
+                minPos = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+PrintArray(arr);
+SortArr(arr);
+PrintArray(arr);
