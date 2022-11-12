82d5efdacb832a653df54235a57ae4e6ea2096f9
Kate <c.chistova2014@yandex.ru>
2022-11-12 19:42:00 +0300

    find factorial

---
+++ Example17_RecursionFactorial/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Факториал {number} = {Factorial(number)}");
+
+
+int Factorial(int num)
+{
+    if(num == 0 || num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * Factorial(num - 1);
+    }
+}
