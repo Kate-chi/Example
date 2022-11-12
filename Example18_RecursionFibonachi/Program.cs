74be1ba27f13e149eba3c5428c57fa79e85d1470
Kate <c.chistova2014@yandex.ru>
2022-11-12 19:56:40 +0300

    fibonachi

---
+++ Example18_RecursionFibonachi/Program.cs
@@ -0,0 +1,16 @@
+﻿for(int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonachi(i));
+}
+
+int Fibonachi(int number)
+{
+    if(number == 1 || number == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonachi(number - 1) + Fibonachi(number - 2);
+    }
+}
